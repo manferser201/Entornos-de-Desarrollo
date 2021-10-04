fb377d0ac9afac8581ff8eb836ca17ef59cab984
manferser201 <alum.mfernandezs.2021@iesalixar.org>
2021-10-04 13:06:21 +0200

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,6 +5,10 @@ Muestra por pntalla la frase Hola mundo
 */
 public class HolaMundo{
 	public static void main(String[] args) {
+	String naranja = "\033[33m";
+	String azul = "\033[34m";
+	
 		System.out.println("Hola mundo");
+		System.out.println("Esta es una nueva versión");
 	}
 }
