db4f69b96435f3aa0c340650582236af6c591d0e
manferser201 <alum.mfernandezs.2021@iesalixar.org>
2021-12-01 09:51:42 +0100

    Esto es una Prueba de concexión de IntelliJ con GitHub

---
+++ Ejemplos/HolaMundo.java
@@ -0,0 +1,14 @@
+/*
+Muestra por pntalla la frase Hola mundo
+
+@author manferser201
+*/
+public class HolaMundo{
+	public static void main(String[] args) {
+	String naranja = "\033[33m";
+	String azul = "\033[34m";
+	
+		System.out.println(naranja + "Hola mundo");
+		System.out.println(azul + "Esta es una nueva versión");
+	}
+}
